705ee149e7ffc45c06ccebba116286337bfa22f2
JuggernOtt81 <juggernott81@gmail.com>
2023-01-06 17:03:28 -0500

    currency marker was showing as ¤ on railway, but needs to be $ as on my test machine

--- Models/LoanPayment.cs
+++ Models/LoanPayment.cs
@@ -9,9 +9,9 @@ namespace ShowcaseMVC.Models
     public class LoanPayment
     {
         public int Month { get; set; }
-        [DisplayFormat(DataFormatString = "{0:C0}", ApplyFormatInEditMode = true)]
+        [DisplayFormat(DataFormatString = "{0:C2}", ApplyFormatInEditMode = true)]
         public decimal Payment { get; set; }
-        [DisplayFormat(DataFormatString = "{0:C0}")]
+        [DisplayFormat(DataFormatString = "{0:C2}")]
         public decimal MonthlyPrinciple { get; set; }
         public decimal MonthlyInterest { get; set; }
         public decimal TotalInterest { get; set; }
